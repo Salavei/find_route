3b49a546ef12c54b43a29a5fce2352f0675caf11
Salavei <kano.mori@mail.ru>
2022-03-24 00:07:54 +0300

    create view,update and detail class generic.Create new templates

---
+++ cities/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from cities.models import City
+
+
+
+
+# class HtmlForm(forms.Form):
+#     name = forms.CharField(label='Город')
+
+
+class CityForm(forms.ModelForm):
+    name = forms.CharField(label='Город', widget=forms.TextInput(attrs={
+        'placeholder': 'Введите название города',
+        'class': 'form-control'
+    }))
+
+    class Meta:
+        model = City
+        fields = ('name',)
+
--- cities/views.py
+++ cities/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.urls import reverse_lazy
 from cities.models import City
 from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from cities.forms import CityForm
 
-__all__ = ('home', 'CityDetailView')
+__all__ = ('home', 'CityDetailView', 'CityCreateView', 'CityUpdateView')
 
 
 def home(request, pk=None):
-    if pk:
-        # city = City.objects.filter(id=pk).first()
-        city = get_object_or_404(City, id=pk)
-        context = {'object': city}
-        return render(request, 'cities/detail.html', context)
+    form = CityForm()
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        print(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+    # if pk:
+    # city = City.objects.filter(id=pk).first()
+    # city = get_object_or_404(City, id=pk)
+    # context = {'object': city}
+    # return render(request, 'cities/detail.html', context)
+
     qs = City.objects.all()
-    context = {'object_list': qs}
+    context = {'object_list': qs, 'form': form}
     return render(request, 'cities/home.html', context)
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    # success_url = '/cities/'
+    success_url = reverse_lazy('cities:home')
 
 
 class CityDetailView(DetailView):
@@ -22,3 +40,9 @@ class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
 
+
+class CityUpdateView(UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:home')
